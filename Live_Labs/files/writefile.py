6626b3833f88dfc83275878f217a59204f5d8267
Vinay <kondojuvinaykumar2004@gmail.com>
2025-12-21 18:12:49 +0530

    Modified Files in Live Labs

--- Live_Labs/files/writefile.py
+++ Live_Labs/files/writefile.py
@@ -1,7 +1,6 @@
-f=open("Live_Labs\Files\Names.txt","w")
+f=open("Live_Labs/Files/Names.txt","w")
 for i in range(3):
     name=input("Enter name: ")
     marks=int(input("Enter marks: "))
     f.write(name+" "+str(marks)+"\n")
-
 f.close()
